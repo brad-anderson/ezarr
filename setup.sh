3922ec084db62e0ecbd963cf30ef9a4dd932e6cf
Brad Anderson <eco@gnuk.net>
2022-09-29 23:15:23 -0600

    Customize a little

--- setup.sh
+++ setup.sh
@@ -8,7 +8,9 @@ sudo useradd readarr -u 13004
 sudo useradd mylar -u 13005
 sudo useradd prowlarr -u 13006
 sudo useradd qbittorrent -u 13007
-sudo useradd jackett -u 13008
+#sudo useradd jackett -u 13008
+sudo useradd bazarr -u 13009
+sudo useradd sabnzbd -u 13010
 sudo groupadd mediacenter -g 13000
 sudo usermod -a -G mediacenter sonarr
 sudo usermod -a -G mediacenter radarr
@@ -17,11 +19,14 @@ sudo usermod -a -G mediacenter readarr
 sudo usermod -a -G mediacenter mylar
 sudo usermod -a -G mediacenter prowlarr
 sudo usermod -a -G mediacenter qbittorrent
-sudo usermod -a -G mediacenter jackett
+#sudo usermod -a -G mediacenter jackett
+sudo usermod -a -G mediacenter bazarr
+sudo usermod -a -G mediacenter sabnzbd
 
 # Make directories
-sudo mkdir -pv docker/{sonarr,radarr,lidarr,readarr,mylar,prowlarr,qbittorrent,jackett}-config
-sudo mkdir -pv data/{torrents,media}/{tv,movies,music,books,comics}
+#sudo mkdir -pv docker/{sonarr,radarr,lidarr,readarr,mylar,prowlarr,qbittorrent,jackett}-config
+sudo mkdir -pv docker/{sonarr,radarr,lidarr,readarr,mylar,prowlarr,qbittorrent,bazarr,sabnzbd}-config
+sudo mkdir -pv data/{torrents,usenet,media}/{tv,movies,music,books,comics}
 
 # Set permissions
 sudo chmod -R 775 data/
@@ -33,6 +38,6 @@ sudo chown -R readarr:mediacenter docker/readarr-config
 sudo chown -R mylar:mediacenter docker/mylar-config
 sudo chown -R prowlarr:mediacenter docker/prowlarr-config
 sudo chown -R qbittorrent:mediacenter docker/qbittorrent-config
-sudo chown -R jackett:mediacenter docker/jackett-config
+#sudo chown -R jackett:mediacenter docker/jackett-config
 
 echo "UID=$(id -u)" >> .env
